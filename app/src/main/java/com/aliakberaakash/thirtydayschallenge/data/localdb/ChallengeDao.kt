4a63d79fb2ad65d04051958195c1c7ecb9dd22cd
Ali Akber <ali852609@gmail.com>
2020-05-29 14:19:58 +0600

    add ChallengeDao class

---
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/localdb/ChallengeDao.kt
@@ -0,0 +1,22 @@
+package com.aliakberaakash.thirtydayschallenge.data.localdb
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+
+@Dao
+interface ChallengeDao {
+
+    @Insert
+    fun insert(challenge: Challenge)
+
+    @Query("SELECT * FROM Challenge")
+    fun getAllChallenge() : List<Challenge>
+
+    @Query("SELECT * FROM Challenge WHERE challengeId=:id")
+    fun getChallenge(id : Int) : List<Challenge>
+
+    @Query("DELETE  FROM Challenge WHERE challengeId=:id")
+    fun deleteChallenge(id : Int) : List<Challenge>
+}
