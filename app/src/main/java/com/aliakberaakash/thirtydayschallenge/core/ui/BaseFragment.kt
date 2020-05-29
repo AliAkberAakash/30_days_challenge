2605cdc69183e71aff61d8b2bdfa78dd812b6542
Ali Akber <ali852609@gmail.com>
2020-05-30 03:27:00 +0600

    add BaseFragment

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/ui/BaseFragment.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/ui/BaseFragment.kt
@@ -1,4 +1,51 @@
 package com.aliakberaakash.thirtydayschallenge.core.ui
 
-class BaseFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseFragment<ViewModel: BaseViewModel, Binding: ViewDataBinding> : Fragment(){
+
+    lateinit var viewModel: ViewModel
+    lateinit var binding: Binding
+
+    abstract fun getLayoutId(): Int
+
+    abstract fun setVariables(binding: Binding)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(getViewModelClass())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false)
+        binding.lifecycleOwner = this
+        setVariables(binding)
+        lifecycle.addObserver(viewModel)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
+
+    private fun getViewModelClass(): Class<ViewModel> {
+        val type =
+            (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0]   // index of 0 means first argument of Base class param
+        return type as Class<ViewModel>
+    }
+
 }
