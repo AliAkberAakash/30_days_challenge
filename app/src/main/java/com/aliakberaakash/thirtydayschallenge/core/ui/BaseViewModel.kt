14fe71b28872e4019ff4b818ca4d1c0fc1cea994
Ali Akber <ali852609@gmail.com>
2020-05-30 02:39:58 +0600

    add BaseViewModel

---
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/ui/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.aliakberaakash.thirtydayschallenge.core.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class BaseViewModel(private val myApplication: Application) : AndroidViewModel(myApplication) {
+
+    
+}
