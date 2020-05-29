4585673f54396bdf20f6042a5c62aa75f87f3d7e
Ali Akber <ali852609@gmail.com>
2020-05-30 04:36:48 +0600

    added HomeViewModel

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeViewModel.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeViewModel.kt
@@ -1,7 +1,36 @@
 package com.aliakberaakash.thirtydayschallenge.ui
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.aliakberaakash.thirtydayschallenge.core.ui.BaseViewModel
+import com.aliakberaakash.thirtydayschallenge.data.HomeRepository
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+import com.aliakberaakash.thirtydayschallenge.data.model.ChallengeAndActivity
+import com.aliakberaakash.thirtydayschallenge.data.model.HomeViewModelCallBack
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(myApplication : Application) : BaseViewModel(myApplication), HomeViewModelCallBack {
+
+    private val repository = HomeRepository(myApplication, this)
+
+    private val _challengeList : MutableLiveData<List<Challenge>> = MutableLiveData()
+    val challengeList : LiveData<List<Challenge>>
+        get() = _challengeList
+
+    override fun onReceiveChallenge(challenge: Challenge?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReceivedAllChallenge(myChallengeList: List<Challenge>) {
+        _challengeList.value = myChallengeList
+    }
+
+    override fun onReceivedChallengeAndActivity(challengeAndActivity: ChallengeAndActivity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onReceivedAllChallengeAndActivity(challengeAndActivities: List<ChallengeAndActivity>) {
+        TODO("Not yet implemented")
+    }
+
 }
