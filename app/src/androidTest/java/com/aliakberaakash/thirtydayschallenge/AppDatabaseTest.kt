fa0d333f88580e4d676b9264ca44875f598bbe73
Ali Akber <ali852609@gmail.com>
2020-05-29 17:39:53 +0600

    Added some tests

--- app/src/androidTest/java/com/aliakberaakash/thirtydayschallenge/AppDatabaseTest.kt
+++ app/src/androidTest/java/com/aliakberaakash/thirtydayschallenge/AppDatabaseTest.kt
@@ -5,8 +5,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.aliakberaakash.thirtydayschallenge.core.AppDatabase
 import com.aliakberaakash.thirtydayschallenge.data.localdb.ChallengeDao
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+import junit.framework.Assert.assertEquals
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
 
@@ -32,6 +35,42 @@ class AppDatabaseTest {
     @Throws(IOException::class)
     fun closeDb() {
         db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetChallenge() {
+        val challenge = Challenge()
+        challengeDao.insert(challenge)
+        val returnedChallenge = challengeDao.getChallenge(1)
+        assertEquals(returnedChallenge.challengeId, 1)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertMultipleAndGetAllChallenge() {
+        val challenge1 = Challenge()
+        val challenge2 = Challenge()
+
+        challengeDao.insert(challenge1)
+        challengeDao.insert(challenge2)
+
+        val challengeList = challengeDao.getAllChallenge()
+        assertEquals(challengeList.size, 2)
+        assertEquals(challengeList[0].challengeId, 1)
+        assertEquals(challengeList[1].challengeId, 2)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndDeleteChallenge() {
+        val challenge = Challenge()
+
+        challengeDao.insert(challenge)
+        challengeDao.deleteChallenge(1)
+
+        val challengeList = challengeDao.getAllChallenge()
+        assertEquals(challengeList.size, 0)
     }
 
 }
