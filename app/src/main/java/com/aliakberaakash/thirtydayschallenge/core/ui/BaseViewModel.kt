c901592fac766fb4326a235c463ea8fe084009e1
Ali Akber <ali852609@gmail.com>
2020-05-30 03:23:03 +0600

    implement IViewModel in BaseViewModel

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/ui/BaseViewModel.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/ui/BaseViewModel.kt
@@ -2,8 +2,39 @@ package com.aliakberaakash.thirtydayschallenge.core.ui
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import com.orhanobut.logger.Logger
 
-class BaseViewModel(private val myApplication: Application) : AndroidViewModel(myApplication) {
-    
-    
+
+class BaseViewModel(private val myApplication: Application) : AndroidViewModel(myApplication), IViewModel {
+    override fun onAny(owner: LifecycleOwner?, event: Lifecycle.Event?) {
+        Logger.d("${event?.name}")
+    }
+
+
+    override fun onCreate(){
+
+    }
+
+    override fun onDestroy(){
+
+    }
+
+    override fun onStart(){
+
+    }
+
+    override fun onStop(){
+
+    }
+
+    override fun onResume(){
+
+    }
+
+    override fun onPause(){
+
+    }
+
 }
