76330ff640bea4fa6f077ee7c0c65cb5ca2b0ffa
Ali Akber <ali852609@gmail.com>
2020-05-29 16:24:34 +0600

    move ChallengeAndActivity related functions to ChallengeDao

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/localdb/ChallengeDao.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/localdb/ChallengeDao.kt
@@ -3,7 +3,9 @@ package com.aliakberaakash.thirtydayschallenge.data.localdb
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+import com.aliakberaakash.thirtydayschallenge.data.model.ChallengeAndActivity
 
 @Dao
 interface ChallengeDao {
@@ -19,4 +21,12 @@ interface ChallengeDao {
 
     @Query("DELETE  FROM Challenge WHERE challengeId=:id")
     fun deleteChallenge(id : Int)
+
+    @Transaction
+    @Query("SELECT * FROM Challenge")
+    fun getAllChallengeAndActivity(): List<ChallengeAndActivity>
+
+    @Transaction
+    @Query("SELECT * FROM Challenge WHERE challengeId=:id")
+    fun getChallengeAndActivity(id : Int) : ChallengeAndActivity
 }
