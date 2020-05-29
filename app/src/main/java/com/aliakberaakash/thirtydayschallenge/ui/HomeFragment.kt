3093689a969bb04ae82228e1fd679c2c2bc650cd
Ali Akber <ali852609@gmail.com>
2020-05-30 05:59:50 +0600

    AddeHomeFragment

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeFragment.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeFragment.kt
@@ -1,33 +1,15 @@
 package com.aliakberaakash.thirtydayschallenge.ui
 
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
 import com.aliakberaakash.thirtydayschallenge.R
+import com.aliakberaakash.thirtydayschallenge.core.ui.BaseFragment
+import com.aliakberaakash.thirtydayschallenge.databinding.HomeFragmentBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel, HomeFragmentBinding>() {
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
+    override fun getLayoutId() = R.layout.home_fragment
 
-    private lateinit var viewModel: HomeViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.home_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun setVariables(binding: HomeFragmentBinding) {
+        binding.viewModel = viewModel
     }
 
 }
