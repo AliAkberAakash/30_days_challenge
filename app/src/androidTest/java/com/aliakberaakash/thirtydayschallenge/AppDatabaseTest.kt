ecfd629d510f8667b115d2b12a8b7c7d19c3381f
Ali Akber <ali852609@gmail.com>
2020-05-29 14:08:52 +0600

    add test code to AppDatabasetest

--- app/src/androidTest/java/com/aliakberaakash/thirtydayschallenge/AppDatabaseTest.kt
+++ app/src/androidTest/java/com/aliakberaakash/thirtydayschallenge/AppDatabaseTest.kt
@@ -1,4 +1,48 @@
 package com.aliakberaakash.thirtydayschallenge
 
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.aliakberaakash.thirtydayschallenge.core.AppDatabase
+import com.aliakberaakash.thirtydayschallenge.data.localdb.ChallengeDao
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
 class AppDatabaseTest {
+
+    private lateinit var challengeDao: ChallengeDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+        challengeDao = db.challengeDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetChallenge() {
+        val challenge : Challenge
+        /*challeg.insert(night)
+        val tonight = sleepDao.getTonight()
+        assertEquals(tonight?.sleepQuality, -1)*/
+    }
+
 }
