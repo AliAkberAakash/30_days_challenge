e9386691bdeefcb54a86136c3b8f77b0b5117a10
Ali Akber <ali852609@gmail.com>
2020-05-29 16:25:03 +0600

    refactor AppDatabase to use ChallengeDao

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/AppDatabase.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/AppDatabase.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.aliakberaakash.thirtydayschallenge.data.localdb.ChallengeAndActivityDao
+import com.aliakberaakash.thirtydayschallenge.data.localdb.ChallengeDao
 import com.aliakberaakash.thirtydayschallenge.data.model.Activity
 import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
 
 @Database(entities = [Challenge::class, Activity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
-    abstract val challengeAndActivityDao: ChallengeAndActivityDao
+    abstract val challengeDao: ChallengeDao
 
     companion object {
 
