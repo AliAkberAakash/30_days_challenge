a2568cdc6266ce1cc7ecf13c1f62098ef954c38c
Ali Akber <ali852609@gmail.com>
2020-05-29 13:57:23 +0600

    add App database class

---
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/core/AppDatabase.kt
@@ -0,0 +1,40 @@
+package com.aliakberaakash.thirtydayschallenge.core
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.aliakberaakash.thirtydayschallenge.data.localdb.ChallengeDao
+import com.aliakberaakash.thirtydayschallenge.data.model.Activity
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+
+@Database(entities = [Challenge::class, Activity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract val challengeDatabaseDao: ChallengeDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "challenge_activities_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+
+}
