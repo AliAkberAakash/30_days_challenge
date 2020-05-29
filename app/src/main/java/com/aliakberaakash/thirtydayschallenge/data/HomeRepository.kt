7643b557c8e930cbd464a1f2a5b4ba61fa746d14
Ali Akber <ali852609@gmail.com>
2020-05-29 18:29:09 +0600

    Added HomeRepository and HomeCallback

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/HomeRepository.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/HomeRepository.kt
@@ -1,4 +1,58 @@
 package com.aliakberaakash.thirtydayschallenge.data
 
-class HomeRepository {
+import android.content.Context
+import com.aliakberaakash.thirtydayschallenge.core.AppDatabase
+import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
+import com.aliakberaakash.thirtydayschallenge.data.model.HomeViewModelCallBack
+
+class HomeRepository(private val context: Context, private val callBack: HomeViewModelCallBack) {
+    private var db: AppDatabase = AppDatabase.getInstance(context)
+
+    fun insertChallenge(challenge: Challenge)
+    {
+        Thread{
+            db.challengeDao.insert(challenge)
+        }
+    }
+
+    fun getChallenge(id : Int)
+    {
+        Thread{
+            //challenge might be null if doesn't exist
+            val challenge : Challenge? = db.challengeDao.getChallenge(id)
+            callBack.onReceiveChallenge(challenge)
+        }
+    }
+    fun getAllChallenges()
+    {
+        Thread{
+            val challengeList = db.challengeDao.getAllChallenge()
+            callBack.onReceivedAllChallenge(challengeList)
+        }
+    }
+
+    fun deleteChallenge(id: Int)
+    {
+        Thread{
+            db.challengeDao.deleteChallenge(id)
+        }
+    }
+
+    fun getChallengeAndActivity(id : Int)
+    {
+        Thread{
+            val challengeAndActivity = db.challengeDao.getChallengeAndActivity(id)
+            callBack.onReceivedChallengeAndActivity(challengeAndActivity)
+        }
+    }
+
+    fun getAllChallengeAndActivity()
+    {
+        Thread{
+            val challengeAndActivities = db.challengeDao.getAllChallengeAndActivity()
+            callBack.onReceivedAllChallengeAndActivity(challengeAndActivities)
+        }
+    }
+
+
 }
--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/model/HomeViewModelCallBack.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/data/model/HomeViewModelCallBack.kt
@@ -1,4 +1,10 @@
 package com.aliakberaakash.thirtydayschallenge.data.model
 
 interface HomeViewModelCallBack {
+
+    fun onReceiveChallenge(challenge: Challenge?)
+    fun onReceivedAllChallenge(challengeList : List<Challenge>)
+    fun onReceivedChallengeAndActivity(challengeAndActivity: ChallengeAndActivity)
+    fun onReceivedAllChallengeAndActivity(challengeAndActivities: List<ChallengeAndActivity>)
+
 }
