ce7407d4d539a5a35fb8e969251dd34fb7173fc6
Ali Akber <ali852609@gmail.com>
2020-05-30 16:03:05 +0600

    add code to fetch challenges

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeFragment.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.aliakberaakash.thirtydayschallenge.ui
 
+import androidx.lifecycle.Observer
 import com.aliakberaakash.thirtydayschallenge.R
 import com.aliakberaakash.thirtydayschallenge.core.ui.BaseFragment
 import com.aliakberaakash.thirtydayschallenge.data.model.Challenge
 import com.aliakberaakash.thirtydayschallenge.databinding.HomeFragmentBinding
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.home_fragment.*
 import kotlinx.android.synthetic.main.home_fragment.view.*
+
 
 class HomeFragment : BaseFragment<HomeViewModel, HomeFragmentBinding>(), BottomSheetCallBack{
 
@@ -13,6 +16,12 @@ class HomeFragment : BaseFragment<HomeViewModel, HomeFragmentBinding>(), BottomS
 
     override fun setVariables(binding: HomeFragmentBinding) {
         binding.viewModel = viewModel
+
+        viewModel.getAllChallenge()
+
+        viewModel.challengeList.observe(viewLifecycleOwner, Observer {
+            Logger.d(it.toString())
+        })
 
         binding.root.add_new_challenge.setOnClickListener {
             val bottomSheetDialog = BottomSheetDialog(this)
