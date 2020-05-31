ef50613de0c523d0716e2760385f6db2e5955886
Ali Akber <ali852609@gmail.com>
2020-05-31 18:29:10 +0600

    add code to recieve arguments in DetailsFragment

--- app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/aliakberaakash/thirtydayschallenge/ui/details/DetailsFragment.kt
@@ -1,12 +1,16 @@
 package com.aliakberaakash.thirtydayschallenge.ui.details
 
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.fragment.navArgs
 
 import com.aliakberaakash.thirtydayschallenge.R
 import com.aliakberaakash.thirtydayschallenge.core.ui.BaseFragment
 import com.aliakberaakash.thirtydayschallenge.databinding.DetailsFragmentBinding
 
 class DetailsFragment : BaseFragment<DetailsViewModel, DetailsFragmentBinding>() {
+
+    val args : DetailsFragmentArgs by navArgs()
 
     override fun getLayoutId() : Int = R.layout.details_fragment
 
@@ -16,6 +20,8 @@ class DetailsFragment : BaseFragment<DetailsViewModel, DetailsFragmentBinding>()
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        Log.d("tag", args.challengeId.toString())
 
     }
 
